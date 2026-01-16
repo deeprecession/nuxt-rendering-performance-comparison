cef6d0cb103e5ddb6899c9026050152ecce2d068
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2026-01-16 13:38:03 +0300

    feat: add navigation

---
+++ components/app/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="container">
+    <ul>
+      <li><strong>Table App</strong></li>
+    </ul>
+    <ul>
+      <li><NuxtLink :to="{ name: 'table' }">Table</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
