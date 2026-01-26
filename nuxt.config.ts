b572af2d33c3c4e45eb006e5f0859ac136a15e0d
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2026-01-26 22:29:06 +0300

    feat: add /table/csr route with CSR table

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
 	modules: ["@nuxt/eslint"],
 	devtools: { enabled: true },
 	css: ["@picocss/pico"],
+	routeRules: {
+		"/table/csr": { ssr: false },
+	},
 	compatibilityDate: "2025-07-15",
 	eslint: {
 		config: {
--- pages/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<h1>My page</h1>
-</template>
