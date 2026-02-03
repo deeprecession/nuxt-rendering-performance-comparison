59675cc3a4edc569ca4263c5deecde54e90c25f0
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2026-02-03 12:05:24 +0300

    refactor: move table to a separate component from pages

--- pages/table/csr.vue
+++ pages/table/csr.vue
@@ -1,45 +1,14 @@
 <template>
-	<article
-		v-if="status ==='pending'"
-		aria-busy="true"
-	/>
-	<article v-else-if="status ==='error'">
-		{{ error }}
-	</article>
-	<article v-else>
-		<h1>{{ tableData?.name }}</h1>
-		<table>
-			<thead>
-				<tr>
-					<th
-						v-for="column in tableData?.columns"
-						:key="column"
-					>
-						{{ column }}
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr
-					v-for="row in tableData?.rows"
-					:key="row.join(',')"
-				>
-					<td
-						v-for="cell in row"
-						:key="cell"
-					>
-						{{ cell }}
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</article>
+	<div>
+		<h1>Client Side Rendering (CSR)</h1>
+		<Table />
+	</div>
 </template>
 
 <script lang="ts" setup>
+import Table from "~/components/app/Table.vue";
+
 definePageMeta({
 	name: "table-csr",
 });
-
-const { data: tableData, error, status } = await useFetch("/api/table", { lazy: true });
 </script>
