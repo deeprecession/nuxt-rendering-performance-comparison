33d93a49f9a3c8d85358286e34de16076eecd033
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2026-02-18 10:20:11 +0300

    feat: add env variables to unlighthouse

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,15 +1,28 @@
 import { defineUnlighthouseConfig } from "unlighthouse/config";
 
+const LOCAL_URL = "http://localhost:3000";
+const PROD_URL = "https://nuxt-rendering-performance-comparis.vercel.app";
+
+const SITE_URL = process.env.NODE_ENV === "PROD" ? PROD_URL : LOCAL_URL;
+const INCLUDE_PATHS = process.env.INCLUDE_PATHS ? process.env.INCLUDE_PATHS.split(",") : [];
+
 export default defineUnlighthouseConfig({
-	site: "https://nuxt-rendering-performance-comparis.vercel.app",
+	site: SITE_URL,
 	scanner: {
 		samples: 10,
 		exclude: [
 			"/",
+
 			"/table",
 			"/table/*",
+
 			"/lcp",
+
+			"/tti",
 		],
+		...(INCLUDE_PATHS.length > 0 && {
+			include: INCLUDE_PATHS,
+		}),
 	},
 	lighthouseOptions: {
 		onlyCategories: ["performance"],
